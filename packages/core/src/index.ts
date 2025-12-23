772aa57021bbe8c8c2526faa7bfa393732a6502b
julien <julien@MacBookPro.QLOCOM>
2025-12-23 17:20:04 +0100

    core(build): fix NodeNext dts issues by adding .js extensions in exports and enabling Node types; re-export only consolidated scanner entry

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,1 @@
-export * from './scanner/framework-detector'
-export * from './scanner/asset-detector'
-export * from './scanner/architecture-detector'
-export * from './scanner/index'
+export * from './scanner/index.js'
